2dac373e5d462309295404ac734ee25d2a582f0e
Santosh Gali <santosh.gali@lelesys.com>
2013-07-31 17:09:54 +0530

    [TASK] Implemented searching in node by searchable: TRUE flag
    Relates: #12961
    Sprint: 31
    
    Change-Id: I7e9e2ca0d88aa33cfca4b2f145d7e498311d4850

--- Classes/Lelesys/Plugin/IndexedSearch/Domain/Service/IndexedSearchService.php
+++ Classes/Lelesys/Plugin/IndexedSearch/Domain/Service/IndexedSearchService.php
@@ -20,6 +20,12 @@ class IndexedSearchService {
 	protected $nodeSearchService;
 
 	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\TYPO3CR\Domain\Service\NodeTypeManager
+	 */
+	protected $nodeTypeManager;
+
+	/**
 	 * Search for all properties in node for given term
 	 *
 	 * @param string $searchParameter
@@ -27,20 +33,58 @@ class IndexedSearchService {
 	 * @return array
 	 */
 	public function search($searchParameter, \TYPO3\TYPO3CR\Domain\Model\Node $currentNode) {
-		$searchNodeTypes = array ('TYPO3.Neos.NodeTypes:Text', 'TYPO3.Neos.NodeTypes:Html', 'TYPO3.Neos.NodeTypes:Headline');
-		$nodes = $this->nodeSearchService->findByProperties($searchParameter, $searchNodeTypes);
+		$currentLocale = (string)$currentNode->getContext()->getLocale();
+		$nodes = $this->nodeSearchService->findByProperties($searchParameter, $this->getSearchabelNodeTypes());
 		$results = array();
 		foreach ($nodes as $node) {
-			$resultNode = $node;
+			$properties = $node->getProperties();
+			foreach ($properties as $propetyName) {
+				if (is_array($propetyName)) {
+					if (strpos(strip_tags($propetyName[$currentLocale]), $searchParameter) !== false) {
+						$searchNode = strip_tags($propetyName[$currentLocale]);
+					}
+				} else {
+					$searchNode = strip_tags($propetyName);
+				}
+			}
 			while ($node !== NULL && (string) $node->getNodeType() !== 'TYPO3.Neos:Page') {
 				$node = $node->getParent();
 				$pageNode = $currentNode->getNode($node->getPath());
 			}
-			$results[] = array('searchNode' => $resultNode, 'pageNode' => $pageNode);
+			$pageProperties = $pageNode->getProperties();
+			if (is_array($pageProperties['title'])) {
+				$pageTitle = $pageProperties['title'][$currentLocale];
+			} else {
+				$pageTitle = $pageProperties['title'];
+			}
+			if (isset($searchNode) && isset($pageTitle)) {
+				$results[] = array('searchNode' => $searchNode, 'pageNode' => $pageNode, 'pageTitle' => $pageTitle);
+			}
 		}
 		return $results;
 	}
 
+	/**
+	 * Get the searchable nodeTypes
+	 *
+	 * @return array
+	 */
+	public function getSearchabelNodeTypes() {
+		$nodeTypes = array();
+		$fullConfiguration = $this->nodeTypeManager->getFullConfiguration();
+		foreach ($fullConfiguration as $key => $value) {
+			if (isset($value['properties'])) {
+				foreach ($value['properties'] as $propertyName => $propertyValue) {
+					if (isset($propertyValue['searchable']) && isset($propertyValue['type'])) {
+						if ($propertyValue['searchable'] === TRUE && $propertyValue['type'] === 'string') {
+							$nodeTypes[] = $key;
+						}
+					}
+				}
+			}
+		}
+		return $nodeTypes;
+	}
 }
 
 ?>
